488d6e946611ac6c1ccbb73f63494d842fd44360
gustavopilotto <gustavo.consultoria@outlook.com>
2020-04-23 21:37:33 -0300

    Adição de Objetos

---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Um Usuário pode ter Nenhum ou Muitos Pedidos
+        /// </summary>
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
